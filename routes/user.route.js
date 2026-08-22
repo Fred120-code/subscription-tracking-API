933dd7f89d4202c6316d39ff3f128ba1ba74afeb
Fred120-code <fredayemtsa@gmail.com>
2026-08-22 14:50:57 +0100

    feat:add router

---
+++ routes/user.route.js
@@ -0,0 +1,35 @@
+import { Router } from 'express';
+
+const userRouter = Router();
+
+userRouter.get('/', (req, res) => {
+    res.send({
+        title: 'get all user'
+    })
+})
+
+userRouter.get('/:id', (req, res) => {
+    res.send({
+        title: "get user by id"
+    })
+})
+
+userRouter.post('/', (req, res) => {
+    res.send({
+        title: "create user"
+    })
+})
+
+userRouter.put('/:id', (req, res) => {
+    res.send({
+        title: "update user by id"
+    })
+})
+
+userRouter.delete('/:id', (req, res) => {
+    res.send({
+        title: "deleted user by id"
+    })
+})
+
+export default userRouter;

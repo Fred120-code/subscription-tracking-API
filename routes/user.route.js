3bb6b31a9b0f33371e04c067e03eefbf6709da77
Fred120-code <fredayemtsa@gmail.com>
2026-08-28 13:39:48 +0100

    fix:user auth ctrl

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,31 +1,21 @@
-import { Router } from 'express';
+import {Router} from 'express';
+import {getUser, getUsers} from "../controllers/user.controller.js";
+import {authorize} from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
-userRouter.get('/', (req, res) => {
-    res.send({
-        title: 'get all user'
-    })
-})
-
-userRouter.get('/:id', (req, res) => {
-    res.send({
-        title: "get user by id"
-    })
-})
-
+userRouter.get('/', getUsers)
+userRouter.get('/:id', authorize, getUser)
 userRouter.post('/', (req, res) => {
     res.send({
         title: "create user"
     })
 })
-
 userRouter.put('/:id', (req, res) => {
     res.send({
         title: "update user by id"
     })
 })
-
 userRouter.delete('/:id', (req, res) => {
     res.send({
         title: "deleted user by id"
